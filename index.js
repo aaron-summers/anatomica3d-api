937042508ad611f957663bcbb32ac400d686c94a
Aaron Summers <aaron.summers2797@gmail.com>
2019-09-11 14:35:41 +0100

    cors optimization

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const systemsRoute = require('./Routes/systems')
 
 //middleware
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5050");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5050");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
